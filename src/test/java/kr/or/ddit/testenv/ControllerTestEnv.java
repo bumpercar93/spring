98b891c5d64b24df1c07e5cab2fc7b7467c2964b
KBH <bumpercar93@gmail.com>
2019-07-05 11:38:10 +0900

    spring xml config -> java config dispatcher servlet   - 테스트코드 적용   - web.xml 반영

--- src/test/java/kr/or/ddit/testenv/ControllerTestEnv.java
+++ src/test/java/kr/or/ddit/testenv/ControllerTestEnv.java
@@ -18,12 +18,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import kr.or.ddit.config.spring.ApplicationContext;
+import kr.or.ddit.config.spring.ApplicationDatasourceDev;
+import kr.or.ddit.config.spring.ApplicationTransaction;
+import kr.or.ddit.config.spring.RootContext;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({
-	   "classpath:kr/or/ddit/config/spring/application-context.xml",
-	   "classpath:kr/or/ddit/config/spring/root-context.xml",
-	   "classpath:kr/or/ddit/config/spring/application-datasource-dev.xml",
-	   "classpath:kr/or/ddit/config/spring/application-transaction.xml"})
+//@ContextConfiguration({
+//	   "classpath:kr/or/ddit/config/spring/application-context.xml",
+//	   "classpath:kr/or/ddit/config/spring/root-context.xml",
+//	   "classpath:kr/or/ddit/config/spring/application-datasource-dev.xml",
+//	   "classpath:kr/or/ddit/config/spring/application-transaction.xml"})
+@ContextConfiguration(classes = {ApplicationContext.class, RootContext.class, 
+						ApplicationDatasourceDev.class, ApplicationTransaction.class})
 //일반 자바 환경 -> 웹 환경
 //applicationContext --> 웹 환경의 applicationContext생성 
 @WebAppConfiguration
